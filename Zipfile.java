dfd5394f0b0e5d3b713200cbb9e556bed5b97b61
pra012 <83650177+pra012@users.noreply.github.com>
2021-05-11 11:04:39 +0530

    Create Zipfile.java

---
+++ Zipfile.java
@@ -0,0 +1,26 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class CompressZipFile {
+ public static void main(String[] args) {
+
+  try (FileOutputStream fos = new FileOutputStream("C:/Project_Work/samples/src_sample.zip");
+    ZipOutputStream zos = new ZipOutputStream(fos);
+    FileInputStream in = new FileInputStream("C:/Project_Work/samples/sample.txt");) {
+   ZipEntry ze= new ZipEntry("sample.txt");
+   
+      zos.putNextEntry(ze);
+   byte[] buffer = new byte[1024];
+   int len;
+   while ((len = in.read(buffer)) > 0) {
+    zos.write(buffer, 0, len);
+   }
+
+  } catch (IOException e) {
+   e.printStackTrace();
+  }
+ }
+}
